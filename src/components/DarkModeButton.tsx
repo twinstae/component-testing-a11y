1c798d6e0c4bec738b41903bb7aab173c27aea2b
twinstae <rabolution@gmail.com>
2022-10-03 17:09:45 +0900

    fix: remove import

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
 import MoonIcon from '../icons/MoonIcon';
 
 function DarkModeButton() {
   return (
-    <button type="button" aria-label="현재 다크 모드" className="btn btn-circle">
-			<MoonIcon />
-		</button>
+    <button
+      type="button"
+      aria-label="현재 다크 모드"
+      className="btn btn-circle"
+    >
+      <MoonIcon />
+    </button>
   );
 }
 

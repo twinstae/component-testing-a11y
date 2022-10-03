669675579c898c329cccd2c856ce8c068ad63551
twinstae <rabolution@gmail.com>
2022-10-03 17:09:02 +0900

    remove state

--- src/components/DarkModeButton.test.tsx
+++ src/components/DarkModeButton.test.tsx
@@ -1,17 +1,13 @@
 import { render } from '@testing-library/react';
 import { screen } from '@testing-library/dom';
-import userEvent from '@testing-library/user-event';
 import DarkModeButton from './DarkModeButton';
 
 describe('DarkModeButton', () => {
-  it('다크 모드 버튼을 클릭하면, 라이트 모드로 바뀐다', async () => {
+  it('다크 모드 버튼을 렌더할 수 있다', () => {
     render(<DarkModeButton />);
-    const button = screen.getByRole('button', {
-      name: '현재 다크 모드',
-    });
 
-    await userEvent.click(button);
+    const button = screen.getByRole('button', { name: '현재 다크 모드' });
 
-    expect(button).toHaveAccessibleName('현재 라이트 모드');
+    expect(button).toBeInTheDocument();
   });
 });
--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,25 +1,11 @@
 import { useState } from 'react';
 import MoonIcon from '../icons/MoonIcon';
-import SunIcon from '../icons/SunIcon';
 
 function DarkModeButton() {
-  const [isDark, setIsDark] = useState(true);
-
-  const label = isDark ? '현재 다크 모드' : '현재 라이트 모드';
-
-  function toggleDark() {
-    setIsDark((old) => !old);
-  }
-
   return (
-    <button
-      type="button"
-      aria-label={label}
-      onClick={() => toggleDark()}
-      className="btn btn-circle btn-primary border-2"
-    >
-      {isDark ? <MoonIcon /> : <SunIcon />}
-    </button>
+    <button type="button" aria-label="현재 다크 모드" className="btn btn-circle">
+			<MoonIcon />
+		</button>
   );
 }
 

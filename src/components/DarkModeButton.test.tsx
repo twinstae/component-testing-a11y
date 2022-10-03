702bb8d413d23f6981aa3b54cd844abe2536ea90
twinstae <rabolution@gmail.com>
2022-10-03 17:13:34 +0900

    implement darkmode state and effect

--- src/components/DarkModeButton.test.tsx
+++ src/components/DarkModeButton.test.tsx
@@ -1,13 +1,28 @@
 import { render } from '@testing-library/react';
 import { screen } from '@testing-library/dom';
 import DarkModeButton from './DarkModeButton';
+import userEvent from '@testing-library/user-event';
 
 describe('DarkModeButton', () => {
-  it('다크 모드 버튼을 렌더할 수 있다', () => {
+  const $html = document.getElementsByTagName('html')[0];
+
+  it('다크 모드 버튼을 클릭하면, 라이트 모드로 바뀌고, 다시 클릭하면 다크 모드로 돌아온다', async () => {
+    const user = userEvent.setup();
     render(<DarkModeButton />);
 
-    const button = screen.getByRole('button', { name: '현재 다크 모드' });
+    expect($html).toHaveAttribute('data-theme', 'forest');
 
-    expect(button).toBeInTheDocument();
+    const button = screen.getByRole('button', { name: '현재 다크 모드' });
+    await user.click(button);
+
+    expect(button).toHaveAccessibleName('현재 라이트 모드');
+    expect($html).toHaveAttribute('data-theme', 'emerald');
+
+    await user.click(button);
+    expect(button).toHaveAccessibleName('현재 다크 모드');
+    expect($html).toHaveAttribute('data-theme', 'forest');
+
+    // 정리
+    $html.setAttribute('data-theme', '');
   });
 });
--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,15 +1,29 @@
 import MoonIcon from '../icons/MoonIcon';
+import { useEffect, useState } from 'react';
+import SunIcon from '../icons/SunIcon';
 
 function DarkModeButton() {
+  const [isDark, setIsDark] = useState(true);
+
+  const label = isDark ? '현재 다크 모드' : '현재 라이트 모드';
+
+  function toggleDark() {
+    setIsDark((old) => !old);
+  }
+
+  useEffect(() => {
+    const $html = document.getElementsByTagName('html')[0];
+    $html.setAttribute('data-theme', isDark ? 'forest' : 'emerald');
+  }, [isDark]);
   return (
     <button
       type="button"
-      aria-label="현재 다크 모드"
+      aria-label={label}
+      onClick={() => toggleDark()}
       className="btn btn-circle"
     >
-      <MoonIcon />
+      {isDark ? <MoonIcon /> : <SunIcon />}
     </button>
   );
 }
-
 export default DarkModeButton;
